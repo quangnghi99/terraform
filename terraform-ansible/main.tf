09e8c054ec5e8f7c8266853f521311d984d5b00c
quangnghi99 <quangnghi2402@gmail.com>
2023-02-09 15:20:11 +0700

    update

--- terraform-ansible/main.tf
+++ terraform-ansible/main.tf
@@ -1,14 +1,20 @@
 provider "aws" {
-    region = "us-west-2"
+  region  = "us-west-2"
 }
 
 resource "tls_private_key" "key" {
-    algorithm = "RSA"
+  algorithm = "RSA"
+}
+
+resource "local_sensitive_file" "private_key" {
+  filename        = "${path.module}/ansible.pem"
+  content         = tls_private_key.key.private_key_pem
+  file_permission = "0400"
 }
 
 resource "aws_key_pair" "key_pair" {
-    key_name   = "ansible-key"
-    public_key = tls_private_key.key.public_key_openssh
+  key_name   = "ansible"
+  public_key = tls_private_key.key.public_key_openssh
 }
 
 resource "aws_security_group" "allow_ssh" {
@@ -34,40 +40,48 @@ resource "aws_security_group" "allow_ssh" {
   }
 }
 
-data "aws_ami" "ami" {
+data "aws_ami" "ubuntu" {
   most_recent = true
 
   filter {
     name   = "name"
-    values = ["amzn2-ami-hvm-2.0.*-x86_64-gp2"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
 
   owners = ["amazon"]
 }
 
 resource "aws_instance" "ansible_server" {
-    ami                    = data.aws_ami.ami.id
-    instance_type          = "t2.micro"
-    vpc_security_group_ids = [aws_security_group.allow_ssh.id]
-    key_name               = aws_key_pair.key_pair.key_name
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  key_name               = aws_key_pair.key_pair.key_name
 
-    provisioner "remote-exec" {
-        inline = [
-        "sudo yum update -y",
-        "sudo yum install -y httpd.x86_64",
-        "sudo systemctl start httpd",
-        "sudo systemctl enable httpd"
-        ]
-    }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update -y",
+      "sudo apt install -y software-properties-common",
+      "sudo apt-add-repository --yes --update ppa:ansible/ansible",
+      "sudo apt install -y ansible"
+    ]
 
     connection {
-        type        = "ssh"
-        user        = "ec2-user"
-        private_key = tls_private_key.key.private_key_pem
-        host        = self.public_ip
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = tls_private_key.key.private_key_pem
+      host        = self.public_ip
+    }
+  }
+
+  provisioner "local-exec" {
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ubuntu --key-file ansible.pem -T 300 -i '${self.public_ip},', playbook.yaml"
     }
 
-    tags = {
-        Name = "Apache Server"
-    }
+  tags = {
+    Name = "Ansible Server"
+  }
+}
+
+output "ec2" {
+  value = aws_instance.ansible_server.public_ip
 }
